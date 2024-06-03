252f3887bbd61b00b7037e3a8daa85d0045948d4
soffet713 <30372648+soffet713@users.noreply.github.com>
2024-06-03 16:14:42 -0400

    Add files via upload

---
+++ IPostsService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionExample
+{
+    public interface IPostsService
+    {
+        Task<IEnumerable<Post>> GetPosts();
+    }
+}
